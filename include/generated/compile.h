81bf696a16ac57401f0d2de3de10642b3ec6a2e1
arnaudmeauzoone <arnaud.meauzoone@gmail.com>
2019-11-06 12:54:28 +0100

    initial

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#3 SMP PREEMPT Wed Nov 6 12:39:19 CET 2019"
+#define LINUX_COMPILE_BY "user"
+#define LINUX_COMPILE_HOST "user-GE62-6QD"
+#define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
